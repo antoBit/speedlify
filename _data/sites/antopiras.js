a2e653d4416c40632e7d2c280624cc4a99f36090
Antonio Piras <antopiras89@gmail.com>
2021-03-07 18:39:29 +0100

    Add antopiras.dev

---
+++ _data/sites/antopiras.js
@@ -0,0 +1,15 @@
+module.exports = {
+	name: "antoDev",
+	description: "Yet another blog where a developer rants",
+	skip: false,
+	options: {
+		frequency: 60 * 23,
+	},
+	urls: [
+		"https://www.antopiras.dev/",
+		"https://www.antopiras.dev/blog/",
+		"https://www.antopiras.dev/blog/2021-02-15-obligatory-first-post/",
+		"https://www.antopiras.dev/blog/2021-03-01-11ty-sass-with-gulp-sass/",
+		"https://www.antopiras.dev/tags/tips/"
+	]
+};
