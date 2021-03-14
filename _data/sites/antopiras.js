665533fe6e61577412b0546ea2f614e02760de38
Antonio Piras <antopiras89@gmail.com>
2021-03-14 12:27:16 +0100

    Update site links

--- _data/sites/antopiras.js
+++ _data/sites/antopiras.js
@@ -8,8 +8,9 @@ module.exports = {
 	urls: [
 		"https://www.antopiras.dev/",
 		"https://www.antopiras.dev/blog/",
+		"https://www.antopiras.dev/tags/tips/",
 		"https://www.antopiras.dev/blog/2021-02-15-obligatory-first-post/",
 		"https://www.antopiras.dev/blog/2021-03-01-11ty-sass-with-gulp-sass/",
-		"https://www.antopiras.dev/tags/tips/"
+		"https://www.antopiras.dev/blog/2021-03-13-w-s-and-no-im-not-swearing/"
 	]
 };
